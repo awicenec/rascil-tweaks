cddfbed8f371c46878b7cad973b7e6f885268fc8
Andreas Wicenec <awicenec@gmail.com>
2022-05-20 16:14:05 +0800

    back to square 0

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setup(
     author="awicenec",
     packages=find_packages(exclude=["tests", ".github"]),
     data_files=[
-        (glob('lib/python*/site-packages')[0]+"/data/models", ["data/models/GLEAM_EGC.fits"]),
-        (glob('lib/python*/site-packages')[0]+"/data/configurations",glob("data/configurations/*"))
+        ("data/models", ["data/models/GLEAM_EGC.fits"]),
+        ("data/configurations",glob("data/configurations/*"))
         ],
     install_requires=read_requirements("requirements.txt"),
     extras_require={"test": read_requirements("requirements-test.txt")},
